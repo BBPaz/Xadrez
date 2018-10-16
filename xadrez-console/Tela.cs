b29a83b3efc5256b2b1102752c6fc9ada61fe426
Bruno <bruno.batista.lp@gmail.com>
2018-10-16 13:28:17 -0300

    Inicio da criação de exibição do tabuleiro

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for(int j = 0; j < tab.colunas; j++)
+                {
+                    Console.Write(tab.peca(i, j) == null ?" -":" "+tab.peca(i,j).ToString());
+                    Console.Write(j==tab.colunas-1?"\n":"");
+                }
+            }
+        }
+        
+    }
+}
